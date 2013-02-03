d233dcdc5ff277b329f8ec1cc5147cb282b61cdc
Kirill Nesterovich <kirill.nesterovich@gmail.com>
2013-02-03 04:57:54 +0400

    Added logic getting player ratings

--- Source/PokerDashboard/App_Code/Repository/PlayerRatingRepository.cs
+++ Source/PokerDashboard/App_Code/Repository/PlayerRatingRepository.cs
@@ -2,6 +2,8 @@
 
 namespace PokerDashboard.Repository
 {
+    using System.Linq;
+    using PokerDashboard.Data;
     using PokerDashboard.Models;
 
     /// <summary>
@@ -15,7 +17,40 @@ namespace PokerDashboard.Repository
         /// <returns>The <see cref="List{PlayerRating}"/>.</returns>
         public List<PlayerRating> Get()
         {
-            return new List<PlayerRating>();
+            using (PokerDashboardDB context = new PokerDashboardDB())
+            {
+                return
+                   (from player in context.Players
+                    join gamePlayer in context.GamePlayers on player.Id equals gamePlayer.PlayerId
+                    join game in context.Games on gamePlayer.GameId equals game.Id
+                    group player by 
+                        new
+                        {
+                            player.Id,
+                            player.Name,
+                            Balance = player.GamePlayers.Sum(gp => gp.Prize) - player.GamePlayers.Sum(gp => gp.Game.BuyInValue),
+                            GamesPlayed = player.GamePlayers.Count(),
+                            GamesFirstPlace = player.GamePlayers.Count(gp => gp.Rank == 1),
+                            GamesSecondPlace = player.GamePlayers.Count(gp => gp.Rank == 2),
+                            GamesThirdPlace = player.GamePlayers.Count(gp => gp.Rank == 3)
+                        }
+                        into ratings
+                        orderby ratings.Key.Balance descending
+                        select ratings).AsEnumerable()
+                    .Select(
+                        (rating, index) =>
+                            new PlayerRating
+                            {
+                                Id = (int)rating.Key.Id,
+                                Name = rating.Key.Name,
+                                GamesPlayed = rating.Key.GamesPlayed,
+                                Balance = (int)rating.Key.Balance,
+                                GamesFirstPlace = rating.Key.GamesFirstPlace,
+                                GamesSecondPlace = rating.Key.GamesSecondPlace,
+                                GamesThirdPlace = rating.Key.GamesThirdPlace,
+                                Rank = index + 1
+                            }).ToList();
+            }
         }
     }
 }
